54e99a90c73b037d7cbe6048f5740dead9d4cbbe
leonbos1 <ktmleon@live.nl>
2023-07-13 21:29:46 +0200

    Updated schedules

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=21)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=7)
+def morning_scheduled_job():
+    os.system("python3 scrape.py")
 
-def scheduled_job():
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=19)
+def evening_scheduled_job():
     os.system("python3 scrape.py")
 
 sched.start()
