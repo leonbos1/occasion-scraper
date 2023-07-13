dea6811bbbcbc58e529d40841efd95b493668b9a
leonbos1 <ktmleon@live.nl>
2023-07-13 21:11:47 +0200

    Changed CRON to 21 o clock

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18, minute=46)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=21)
 
 def scheduled_job():
     os.system("python3 scrape.py")
