63e81999a346ad9d10d4c0ea14f2e53a49bbb7c3
leonbos1 <76918287+leonbos1@users.noreply.github.com>
2023-07-20 22:02:00 +0200

    Turned headless on

--- frontend/src/components/cars/Cars.vue
+++ frontend/src/components/cars/Cars.vue
@@ -2,6 +2,14 @@
   <div class="w-full text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
     <div class="flex flex-col items-center justify-center w-full h-32">
       <perPageComponent class="w-1/4 mr-auto" @option-selected="perPageSelected" />
+      <paginationComponent
+        class="w-1/4 ml-auto"
+        :current-page="currentPage"
+        :has-next="hasNext"
+        :has-previous="hasPrevious"
+        @previous="handlePrevious"
+        @next="handleNext"
+      />
     </div>
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table class="w-full text-sm text-left">
@@ -36,20 +44,56 @@
 import { ref, onMounted, watch } from 'vue';
 import CarRow from './CarRow.vue';
 import perPageComponent from '../datatables/perPageComponent.vue';
+import paginationComponent from '../datatables/paginationComponent.vue';
 
 const cars = ref([]);
 const selectedCars = ref([]);
 const perPage = ref(10);
+const currentPage = ref(1);
+const hasNext = ref(false);
+const hasPrevious = ref(false);
 
 function perPageData() {
-  return cars.value.slice(0, perPage.value);
+  const start = (currentPage.value - 1) * perPage.value;
+  const end = currentPage.value * perPage.value;
+  return cars.value.slice(start, end);
 }
 
 function perPageSelected(option) {
   perPage.value = option;
 };
 
+function handlePrevious() {
+  currentPage.value--;
+
+  if (currentPage.value === 1) {
+    hasPrevious.value = false;
+  }
+
+  if (currentPage.value < cars.value.length / perPage.value) {
+    hasNext.value = true;
+  }
+}
+
+function handleNext() {
+  console.log("parent")
+  console.log(currentPage.value)
+  currentPage.value++;
+
+  if (currentPage.value > 1) {
+    hasPrevious.value = true;
+  }
+
+  if (currentPage.value === cars.value.length / perPage.value) {
+    hasNext.value = false;
+  }
+}
+
 watch(perPage, () => {
+  selectedCars.value = perPageData();
+});
+
+watch(currentPage, () => {
   selectedCars.value = perPageData();
 });
 
---
+++ frontend/src/components/datatables/paginationComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <button
+            class="px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 bg-white border border-gray-300 rounded-md active:bg-gray-100 hover:bg-gray-100 focus:outline-none focus:shadow-outline-blue"
+            @click="handlePrevious">
+            Previous
+        </button>
+        <button
+            class="px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 bg-white border border-gray-300 rounded-md active:bg-gray-100 hover:bg-gray-100 focus:outline-none focus:shadow-outline-blue"
+            @click="handleNext">
+            Next
+        </button>
+    </div>
+</template>
+  
+<script setup>
+import { defineEmits, ref, watch, withDefaults } from 'vue';
+
+const emit = defineEmits();
+
+function handlePrevious() {
+    emit('previous');
+}
+
+function handleNext() {
+    emit('next');
+}
+
+const hasNext = ref(false);
+const hasPrevious = ref(false);
+
+</script>
+  
