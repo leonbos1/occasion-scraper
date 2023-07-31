85f3d3d509e63db494a5bd561a30728199c76fb3
leonbos1 <76918287+leonbos1@users.noreply.github.com>
2023-07-31 20:08:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import os
+import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
+URL = "https://leonbos.nl:5000/scrape_session/start"
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=7)
 def morning_scheduled_job():
-    os.system("python3 scrape.py")
-
+    response = requests.get(URL)
+    if response.status_code == 200:
+        print("Morning scrape session started successfully.")
+    else:
+        print("Failed to start morning scrape session.")
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=19)
 def evening_scheduled_job():
-    os.system("python3 scrape.py")
-
+    response = requests.get(URL)
+    if response.status_code == 200:
+        print("Evening scrape session started successfully.")
+    else:
+        print("Failed to start evening scrape session.")
 
 sched.start()
