e500a06117e7770fd636713e60c749bfdc8611e2
leonbos1 <76918287+leonbos1@users.noreply.github.com>
2023-08-22 19:37:32 +0200

    Update Home.vue

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <!--make a button to do a get request to leonbos.nl:5000/start
+      and center it-->
+      <div class="flex justify-center">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="start">Start</button>
+      </div>
     </div>
   </template>
   
   <script setup>
   import { ref, onMounted } from 'vue';
 
-  
+  const start = () => {
+    fetch('http://leonbos.nl:5000/start')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  }
   
   </script>
   
