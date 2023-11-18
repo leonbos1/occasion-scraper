4c4b74065d51d9b356e7cf428396fa118f945765
leonbos1 <76918287+leonbos1@users.noreply.github.com>
2023-11-18 16:01:20 +0100

    Fix

--- scrapers/autoscout.py
+++ scrapers/autoscout.py
@@ -153,7 +153,7 @@ def scrape_blueprint(driver: webdriver, cars: list, blueprint: BluePrint):
 
             try:
                 #fix to prevent stale element exception
-                article = main.find_element_by_xpath(f"//article[@data-guid='{id}']")
+                article = main.find_element_by_xpath(f"//article[@id='{id}']")
                 car = Car(id=article.get_attribute("data-guid"), brand=article.get_attribute("data-make"), model=article.get_attribute("data-model"), price=article.get_attribute("data-price"),
                           mileage=mileage, first_registration=convert_to_year(article.get_attribute("data-first-registration")), vehicle_type=article.get_attribute("data-vehicle-type"),
                           location=location, condition=mileage, url=href, session_id=scrape_session.id, image=image)
